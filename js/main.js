ea3ec281a9a90ad3179dc75ce575d12d73effed9
KellerMarika <marikakeller92@gmail.com>
2022-11-05 12:27:47 +0100

    bottoni

--- js/main.js
+++ js/main.js
@@ -47,11 +47,13 @@ for (i = 0; i < images.length; i++) {
     //console.log(active);
     //(ad inizializzazione pagina l'elelmento attivo è il primo perciò)
     if (i === 0) {
-        // active = "active"
+        active = "active"
     }
 
+    const thumbnail_El = "thumbnail-img"
+
     //stampo le immagini con le classi appropriate
-    thumbnailsContainer_El.innerHTML += `<img src=${images[i]} class= ${active}>`;
+    thumbnailsContainer_El.innerHTML += `<img src=${images[i]} class= "${thumbnail_El} ${active} ">`;
 }
 
 
@@ -68,43 +70,145 @@ for (i = 0; i < images.length; i++) {
     if (i === 0) {
         visibility = ""
     }
+
+    const slider_El = "slider-img"
     //stampo le immagini con le classi appropriate
-    sliderContainer_El.innerHTML += `<img src=${images[i]} class= ${visibility}>`;
+    sliderContainer_El.innerHTML += `<img src=${images[i]} class=" ${slider_El} ${visibility}"  >`;
 }
 
 
-/* INIZIO FUNZIONE NEXT  ***************************************/
-console.log(btnNext)
-console.log(btnPrev)
 
-btnPrev.addEventListener("click", function () {
-    counter+=1
+//console.log(btnNext)
+//console.log(btnPrev)
 
+/* recupero il primo elementoimg in "active" stampato in themplates-container */
+
+
+
+
+/* btnPrev.addEventListener("click", function () {
+    // rimuovo active dall'immagine default 
+    oldActiveThumb.classList.remove("active");
+
+    counter += 1
     console.log("btnPrev click");
-})
-btnNext.addEventListener("click", function () {
-    counter+=1
 
-    console.log("btnNext click");
-})
-/* btnPrev */
+    const oldActiveThumb = document.querySelector(`#thumbnails-container :nth-child(${counter + 1})`);
+    console.log(oldActiveThumb);
 
-/* inserisco le immagini nelle thumbnails */
-/* for (i = 0; i < images.length; i++) {
-    console.log(i);
+
+    const currentSliderImg = document.querySelector(`#slider-container :nth-child(${counter + 1})`);
+
+    const oldSliderImg = document.querySelector(`#slider-container :nth-child(${counter})`);
+
+
+
+
+
+});
  */
-/* appena comincia il ciclo*/
-/* creo un elemento */
-/*     const thumbnail_El = document.createElement("div");
-    console.log(thumbnail_El)
-    /* a cui asssegno una classe */
-/*  thumbnail_El.classList.add("thumnails"); */
 
-/*se siamo alla prima posizione del ciclo */
-/*    if (i === 0) {
-       active = "active"
-*/
-/* ne assegno anche un'altra (active stilizzata in css) */
-/*        thumbnail_El.classList.add("active");
-   }
-} */
+
+
+
+
+
+
+
+
+/* INIZIO FUNZIONE NEXT  ***************************************/
+btnNext.addEventListener("click", function () {
+
+    /* a ogni cick incremento il counter di 1. è lo stesso che scrivere counter+=1 */
+    counter += 1
+    console.log("btnNext click", counter)
+
+
+    //lenght è =5 ma il counter parte da 0 quindi l'ultimo valore disponibile è lenght -1
+    //se il contatore del bottone è maggiore dlla lenght di images 
+    if (counter > (images.length - 1)) {
+        // console.log("counter if=", counter, images.length);
+
+        // ricomincia d'accapo___________________________________________________________________________________ci devo ragionare
+        counter = images.length - 1
+        btnNext.classList.add("disabled");
+        //console.log(btnNext);
+    }
+
+    //recupero la thumbnail corrispondente alla posizione start (al click diventa 1)
+    const oldActiveThumb = document.querySelector(`#thumbnails-container :nth-child(${counter})`);
+    //console.log(oldActiveThumb);
+    //recupero la thumbnail corrispondente alla posizione dopo il click (+1)
+    const currentActiveThumb = document.querySelector(`#thumbnails-container :nth-child(${counter + 1})`);
+    //console.log("currentActiveT =", currentActiveThumb);
+
+    // dopo il click rimuovo active dall'immagine 
+    oldActiveThumb.classList.remove("active");
+    // dopo il click rimuovo active dall'immagine default 
+    currentActiveThumb.classList.add("active");
+
+
+    //recupero la slide corrispondente alla posizione start (al click diventa 1)
+    const oldSliderImg = document.querySelector(`#slider-container :nth-child(${counter})`);
+
+    //recupero la slide corrispondente alla posizione dopo il click (+1)
+    const currentSliderImg = document.querySelector(`#slider-container :nth-child(${counter + 1})`);
+
+    // dopo il click rimuovo active dall'immagine 
+    oldSliderImg.classList.add("visually-hidden");
+    //console.log(oldSliderImg, oldSliderImg.classList);
+    // dopo il click rimuovo active dall'immagine default 
+    currentSliderImg.classList.remove("visually-hidden");
+    //console.log(currentSliderImg, currentSliderImg.classList);
+});
+
+
+
+
+
+
+/* INIZIO FUNZIONE PREV  ***************************************/
+btnPrev.addEventListener("click", function () {
+
+    /* a ogni cick incremento il counter di 1. è lo stesso che scrivere counter+=1 */
+    counter -= 1
+    console.log("btnNext click", counter);
+
+
+    //lenght è =5 ma il counter parte da 0 quindi l'ultimo valore disponibile è lenght -1
+    //se il contatore del bottone è maggiore dlla lenght di images 
+    /*     if (counter <0) {
+            console.log("counter if=", counter, images.length);
+    
+            // ricomincia d'accapo___________________________________________________________________________________ci devo ragionare
+            counter = 1
+            btnPrev.classList.add("disabled");
+            console.log(btnPrev);
+        } */
+    //recupero la thumbnail corrispondente alla posizione start (al click diventa 1)
+    const oldActiveThumb = document.querySelector(`#thumbnails-container :nth-child(${counter})`);
+    console.log("oldT", oldActiveThumb);
+    //recupero la thumbnail corrispondente alla posizione dopo il click (+1)
+    const currentActiveThumb = document.querySelector(`#thumbnails-container :nth-child(${counter + 1})`);
+    console.log("currentActiveT =", currentActiveThumb);
+
+    // dopo il click rimuovo active dall'immagine 
+    oldActiveThumb.classList.remove("active");
+    // dopo il click rimuovo active dall'immagine default 
+    currentActiveThumb.classList.add("active")
+    /* 
+    
+        //recupero la slide corrispondente alla posizione start (al click diventa 1)
+        const oldSliderImg = document.querySelector(`#slider-container :nth-child(${counter})`);
+    
+        //recupero la slide corrispondente alla posizione dopo il click (+1)
+        const currentSliderImg = document.querySelector(`#slider-container :nth-child(${counter + 1})`);
+    
+        // dopo il click rimuovo active dall'immagine 
+        oldSliderImg.classList.add("visually-hidden");
+        console.log(oldSliderImg, oldSliderImg.classList);
+        // dopo il click rimuovo active dall'immagine default 
+        currentSliderImg.classList.remove("visually-hidden");
+        console.log(currentSliderImg, currentSliderImg.classList);  */
+});
+
