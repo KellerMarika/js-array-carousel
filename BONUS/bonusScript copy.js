dd419102f98a299f120a832805006d6bdfa233c6
KellerMarika <marikakeller92@gmail.com>
2022-11-07 16:10:45 +0100

    pausa

--- BONUS/bonusScript copy.js
+++ BONUS/bonusScript copy.js
@@ -49,6 +49,41 @@ const imagesAlt = ["panoramica"
     , "pergolato"
 ]
 
+const svgGroups = [
+
+
+    `<div class="svg-container f1">
+
+    <svg class="living f1" width="439" height="430" viewBox="0 0 439 430" fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+
+        <path d="M436.5 3H172H11L6.5 194L105 196V427H271.5V326.5H405.5V162H436.5V3Z" fill="#D9D9D9"
+            fill-opacity="" stroke="" stroke-opacity="" stroke-width="" />
+
+        <path class="bath f1" d="M105.5 196L6.5 194L3 327.5H105.5V196Z" fill="#D9D9D9" fill-opacity="" stroke=""
+            stroke-opacity="" stroke-width="" />
+    </svg>
+</div>`
+
+    , `<div class="svg-container f2">
+            <!---- SECOND SVG GROUP----------->
+            <svg width="440" height="429" viewBox="0 0 440 429" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+                <path class="study f2" d="M405.5 161H270.5V325.5H405.5V161Z" fill="#D9D9D9" fill-opacity="" stroke=""
+                    stroke-width="" stroke-opacity="0" stroke-opacity="" stroke-width="" />
+
+                <path class="room f2" d="M271 160.5V3H437V160.5H271Z" fill="#D9D9D9" fill-opacity="" stroke=""
+                    stroke-width="" stroke-opacity="0" stroke-opacity="" stroke-width="" />
+
+                <path class="corridor f2" d="M105.5 426V195H171.5V2.5H271V426H105.5Z" fill="#D9D9D9" fill-opacity=""
+                    stroke="" stroke-width="" stroke-opacity="0" stroke-opacity="" stroke-width="" />
+
+                <path class="bath f2" d="M105.5 195L6.5 193L3 326.5H105.5V195Z" fill="#D9D9D9" fill-opacity="" stroke=""
+                    stroke-width="" stroke-opacity="0" stroke-opacity="" stroke-width="" />
+            </svg>
+        </div>`
+
+]
 
 //LABEL THUMB
 // create h4 
@@ -56,9 +91,9 @@ const imagesAlt = ["panoramica"
 
 
 /********************************************
-                CLASSI
+                CLASSI (creo nela wile)
 *******************************************/
-//THUMBNAILS:
+/* //THUMBNAILS:
 
 //classe nominale e numerata
 let thumbnail_El //= "thumbnail-card " + (i + 1)
@@ -83,7 +118,14 @@ let thumbnailLabel_El  //= "thumbnail-label" + (i + 1)
 
 // classe funzionale 
 //let visibility= "visually-hidden"  || " "_____________________
+ */
 
+/********************************************
+CREAZIONE THUMB + LABEL && SLIDE + SVG
+*******************************************/
+//thumb=(div.thumbnail-card.active>img[src][alt]+h4.label.visibility)
+
+//slide=(div.slide.visibility>img[src][alt])__________(dopo solo a 2 devo aggiungere le svg (wile 12))
 
 
 // contatore 
@@ -93,13 +135,76 @@ let i = 0
 
 //FINCHE' i risulta < della lunghezza dell'array degli src(5 partendo da 0)
 while (i < imagesSrc.length) {
+    console.log(i);
+
+    //IMG:..........................
+    //creo l'immagine
+    const img_El = document.createElement("img");
+
+    //aggiungo l'src
+    img_El.src = imagesSrc[i]
+    //aggiungo l'alt
+    img_El.alt = imagesAlt[i]
+
+    console.log(img_El);
+
+    //THUMBNAILS:..........................
+
+    const thumbnail_El = document.createElement("div");
+
+    //classe nominale e numerata
+    let thumbnail_ClassName = "thumbnail-card " + (i + 1)
+
+    // classe funzionale 
+    let active = " "
 
 
 
-    while (i === 0) {
 
-        active = "active"
-    }
+
+
+
+
+    //LABEL THUMB........................
+
+    //classe nominale e numerata
+    let thumbnailLabel_ClassName = "thumbnail-label" + (i + 1)
+
+    const thumbnailLabel_El = document.createElement("h4");
+    console.log(thumbnailLabel_El);
+
+    //SLIDERIMGS:..........................
+
+    //classe nominale e numerata
+    let slide_ClassName = "slide" + (i + 1)
+
+    // classe funzionale 
+    let visibility = "visually-hidden"
+
+    const slide_El = document.createElement("div");
+    console.log(slide_El);
+
+
+
+    /*   while (i === 0) {
+  
+          active = "active"
+          visibility = ""
+      } */
+
+    /* 
+        while (i === 1) {
+            //aggiungi svggroup 1
+    
+        }
+    
+        i++
+        while (i === 2) {
+            //aggiungi svg group 2
+    
+        } */
+
+    i++
 
 }
 
